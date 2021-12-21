187a0a845b53aa05452b1737684773a63b54b18b
lispon <lisponsored@gmail.com>
2021-12-21 18:09:32 +0800

    1, 经查看 readthedocs 日志, 发现无法正确找到 rst 文件, 怀疑相对路径错误, 再次尝试.

--- source/conf.py
+++ source/conf.py
@@ -10,10 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -24,17 +23,14 @@ author = 'lispon'
 # The full version, including alpha/beta/rc tags
 release = 'v0.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
-needs_extensions = {
-}
+needs_extensions = {}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,13 +43,12 @@ templates_path = ['_templates']
 language = 'zh_CN'
 
 #. html_search_language=zh, needs jieba. using 'pip install jieba' to install.
-html_search_language='zh'
+html_search_language = 'zh'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
